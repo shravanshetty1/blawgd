79bba2c7a229ccdfab5ab1b16670e7bb03106c90
Shravan Shetty <shravanshetty322@gmail.com>
2021-09-09 16:49:49 +0530

    added visual indicators for reply

--- frontends/rust/client/src/components/post.rs
+++ frontends/rust/client/src/components/post.rs
@@ -24,9 +24,20 @@ impl super::Component for PostComponent {
             post_text_class = "post-component-text-focus";
         }
 
+        let parent_post = self.post.parent_post.clone();
+        let mut post_header: String = String::new();
+        if !parent_post.is_empty() {
+            post_header = format!(
+                r#"<div class="post-component-header">Replying to post {}</div>"#,
+                parent_post
+            )
+            .to_string();
+        }
+
         String::from(format!(
             r#"
         <div class="post-component">
+            {}
             <div class="post-component-text-wrapper">
                 <a href="/profile/{}"><img src="{}" class="post-component-account-info-image"></a>
                 <div class="post-component-text-content">
@@ -45,6 +56,7 @@ impl super::Component for PostComponent {
                 <a href="/post/{}" class="post-component-bar-button"><div class="post-component-bar-button-content">Comment</div></a>
             </div>
         </div>"#,
+            post_header,
             account_info.address,
             account_info.photo,
             account_info.address,
