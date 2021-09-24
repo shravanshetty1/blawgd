4881b1da6ea946bc80c5365f6d841707f1e700cb
Shravan Shetty <shravanshetty322@gmail.com>
2021-09-24 11:17:34 +0530

    100% * refactor

--- frontends/rust/client/src/pages/edit_profile_page.rs
+++ frontends/rust/client/src/pages/edit_profile_page.rs
@@ -10,7 +10,8 @@ use std::sync::Arc;
 impl PageBuilder {
     pub async fn edit_profile_page(ctx: Arc<ApplicationContext>) -> Result<Box<dyn Component>> {
         let session = ctx.session.clone();
-        let account_info_comp = AccountInfoComp::new(session.clone().unwrap());
+        let account_info_comp =
+            AccountInfoComp::new(session.clone().ok_or(anyhow!("user is not logged in"))?);
         let nav_bar = NavBar::new(session.clone());
         let comp = EditProfilePage::new(nav_bar, account_info_comp);
         Ok(comp)
